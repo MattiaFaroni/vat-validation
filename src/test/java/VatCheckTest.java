fa41bcee4ece4826d7d64bae49028019190a3bdd
MattiaFaroni <mattiafaroni@gmail.com>
2025-02-04 21:26:06 +0100

    Added test classes

---
+++ src/test/java/VatCheckTest.java
@@ -0,0 +1,52 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class VatCheckTest {
+
+    @BeforeEach
+    public void setup() {
+        RestAssured.baseURI = "http://localhost:8080";
+        RestAssured.basePath = "/api/vies";
+    }
+
+    @Test
+    public void testValidVatNumber() {
+        String requestBody = "{\"iso2\": \"IT\", \"vatNumber\": \"00159560366\" }";
+
+        RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("/check")
+                .then()
+                .statusCode(200)
+                .body("data.valid", equalTo(true))
+                .body("data.name", equalTo("FERRARI S.P.A."))
+                .body("system.code", equalTo(0))
+                .body("system.description", equalTo("Ok"));
+    }
+
+    @Test
+    public void testInvalidVatNumber() {
+        String requestBody = "{\"iso2\": \"IT\", \"vatNumber\": \"123456789\" }";
+
+        RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("/check")
+                .then()
+                .statusCode(200)
+                .body("data.valid", equalTo(false))
+                .body("system.code", equalTo(0))
+                .body("system.description", equalTo("Ok"));
+    }
+}
---
+++ src/test/java/ViesStatusTest.java
@@ -0,0 +1,32 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.*;
+
+public class ViesStatusTest {
+
+    @BeforeEach
+    public void setup() {
+        RestAssured.baseURI = "http://localhost:8080";
+        RestAssured.basePath = "/api/vies";
+    }
+
+    @Test
+    public void testViesStatus() {
+        RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/status")
+                .then()
+                .statusCode(200)
+                .body("data.size()", greaterThan(0))
+                .body("data[0].iso2", notNullValue())
+                .body("data[0].status", notNullValue())
+                .body("system.code", equalTo(0))
+                .body("system.description", equalTo("Ok"));
+    }
+}
