54adf9574f0616e1fa2451d8e960779b2fbc48b6
MattiaFaroni <mattiafaroni@gmail.com>
2025-02-04 22:03:14 +0100

    Added cors filter

---
+++ src/main/java/com/data/validation/filter/CorsFilter.java
@@ -0,0 +1,24 @@
+package com.data.validation.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(displayName = "CorsFilter", urlPatterns = {"/*"})
+public class CorsFilter implements Filter {
+
+    public void doFilter(ServletRequest req, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS,  DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization,Content-Type, x-requested-with");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setCharacterEncoding("UTF-8");
+
+        chain.doFilter(req, servletResponse);
+    }
+}
